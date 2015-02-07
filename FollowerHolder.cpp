afb2bf5daea557229c413bc796714f99322c5e0e
Steven <ssk97@case.edu>
2015-02-07 06:00:00 -0500

    updated ball

--- FollowerHolder.cpp
+++ FollowerHolder.cpp
@@ -13,11 +13,12 @@ FollowerHolder::~FollowerHolder()
 
 GLuint FollowerHolder::setupGeometry(){
 	GLuint vbo;
-	float vertices[22];
+	const int count = 182;
+	float vertices[count];
 	vertices[0] = 0.0f; vertices[1] = 0.0f;
-	for (int i = 2; i < 22; i += 2){
-		vertices[i] = Dcos(i * 360 / 20)*100.0f;
-		vertices[i + 1] = Dsin(i * 360 / 20)*100.0f;
+	for (int i = 2; i < count; i += 2){
+		vertices[i] = Dcos(i * 360 / count)*100.0f;
+		vertices[i + 1] = Dsin(i * 360 / count)*100.0f;
 	}
 	glGenBuffers(1, &vbo); // Generate 1 buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
