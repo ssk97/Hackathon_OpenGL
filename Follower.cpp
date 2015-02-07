36f824f5e4c353313c32de516a11c0eb8d6945a4
ron <rdw62@case.edu>
2015-02-07 06:35:50 -0500

    Played with the physics a little.

--- Follower.cpp
+++ Follower.cpp
@@ -5,26 +5,32 @@ Follower::Follower(){}
 
 Follower::~Follower(){}
 
-const double SPRING_CONSTANT = 0.001;
+const double SPRING_CONSTANT_SLOW = 0.005;
 const double FRICTION = .99;
-const double SPRING_LENGTH = 20;
+const double SPRING_LENGTH = 25;
 void Follower::update()
 {
     double mouseX, mouseY;
     glfwGetCursorPos(window, &mouseX, &mouseY);
+    int buttonDown = glfwGetMouseButton(window, 0);
 
     double delta_y = mouseY - y;
     double delta_x = mouseX - x;
+
+    double springConstant = buttonDown ? SPRING_CONSTANT_SLOW * 2 : SPRING_CONSTANT_SLOW;
 
     double angleToTarget = atan2(delta_y, delta_x);
     double equilibrium_y = SPRING_LENGTH * sin(angleToTarget) + mouseY;
     double equilibrium_x = SPRING_LENGTH * cos(angleToTarget) + mouseX;
 
-    yAcc = SPRING_CONSTANT * (equilibrium_y - y);
-    xAcc = SPRING_CONSTANT * (equilibrium_x - x);
+    yAcc = springConstant * (equilibrium_y - y);
+    xAcc = springConstant * (equilibrium_x - x);
 
-    xVel *= FRICTION;
-    yVel *= FRICTION;
+    if (!buttonDown)
+    {
+        xVel *= FRICTION;
+        yVel *= FRICTION;
+    }
     xVel += xAcc;
     yVel += yAcc;
 
