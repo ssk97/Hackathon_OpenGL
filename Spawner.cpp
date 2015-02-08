95c8e1cc443fc8e5247b071f3eeaa9124ee7421c
ron <rdw62@case.edu>
2015-02-08 01:45:11 -0500

    Refactored spawning to allow multiple modes.

--- Spawner.cpp
+++ Spawner.cpp
@@ -14,47 +14,63 @@ void Spawner::tick()
 {
 	if (time++ % 250 == 0){
 		wave++;
-        for (int i = 0; i < wave; i++)
-        {
-            ((LineTripperHolder*)(holderArray[LINETRIPPER]))->AddLineTripper();
-            ((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
-        }/*
-		int remainingCount = wave;
-
-		if (wave >= 3){
-			int motherShipCount = (rand() % (remainingCount / 2));
-			for (int i = 0; i < motherShipCount; i++)
-			{
-				((MothershipHolder*)(holderArray[MOTHERSHIP]))->addMothership();
-			}
-			remainingCount -= motherShipCount * 2;
-
-			if (remainingCount > 0)
-			{
-                int lineTripperCount = (rand() % remainingCount / 3);
-                for (int i = 0; i < lineTripperCount; i++)
-                {
-                    ((LineTripperHolder*)(holderArray[LINETRIPPER]))->AddLineTripper();
-                }
-                remainingCount -= lineTripperCount;
-			}
-
-			if (remainingCount > 0)
-			{
-                int carrierCount = (rand() % remainingCount / 3);
-                for (int i = 0; i < carrierCount; i++)
-                {
-                    ((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
-                }
-                remainingCount -= carrierCount * 2;
-			}
-
+		switch(mode)
+		{
+        case Normal:
+            wave_normal();
+            break;
+        case CTRush:
+            wave_CTRush();
+            break;
 		}
-		for (int i = 0; i < remainingCount; i++){
-			((ZombieHolder*)(holderArray[ZOMBIE]))->AddZombie();
-		}*/
 	}
 }
 
+void Spawner::wave_normal()
+{
+    int remainingCount = wave;
+
+    if (wave >= 3){
+        int motherShipCount = (rand() % (remainingCount / 2));
+        for (int i = 0; i < motherShipCount; i++)
+        {
+            ((MothershipHolder*)(holderArray[MOTHERSHIP]))->addMothership();
+        }
+        remainingCount -= motherShipCount * 2;
+
+        if (remainingCount > 0)
+        {
+            int lineTripperCount = (rand() % remainingCount / 3);
+            for (int i = 0; i < lineTripperCount; i++)
+            {
+                ((LineTripperHolder*)(holderArray[LINETRIPPER]))->AddLineTripper();
+            }
+            remainingCount -= lineTripperCount;
+        }
+
+        if (remainingCount > 0)
+        {
+            int carrierCount = (rand() % remainingCount / 3);
+            for (int i = 0; i < carrierCount; i++)
+            {
+                ((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
+            }
+            remainingCount -= carrierCount * 2;
+        }
+
+    }
+    for (int i = 0; i < remainingCount; i++){
+        ((ZombieHolder*)(holderArray[ZOMBIE]))->AddZombie();
+    }
+}
+
+void Spawner::wave_CTRush()
+{
+    for (int i = 0; i < wave; i++)
+    {
+        ((LineTripperHolder*)(holderArray[LINETRIPPER]))->AddLineTripper();
+        ((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
+    }
+}
 
 
