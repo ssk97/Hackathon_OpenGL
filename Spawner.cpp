154b6f20c7fa85125ac7f727f3820db22fe10ddb
ron <rdw62@case.edu>
2015-02-07 21:14:23 -0500

    Fixed carriers.

--- Spawner.cpp
+++ Spawner.cpp
@@ -15,7 +15,6 @@ void Spawner::tick()
 	if (time++ % 240 == 0){
 		wave++;
 		int remainingCount = wave;
-		((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
 		if (wave >= 3){
 			int motherShipCount = (rand() % (remainingCount / 2));
 			for (int i = 0; i < motherShipCount; i++)
@@ -36,7 +35,7 @@ void Spawner::tick()
 
 			if (remainingCount > 0)
 			{
-                int carrierCount = (rand() % remainingCount / 2);
+                int carrierCount = (rand() % remainingCount / 3);
                 for (int i = 0; i < carrierCount; i++)
                 {
                     ((CarrierHolder*)(holderArray[CARRIER]))->AddCarrier();
